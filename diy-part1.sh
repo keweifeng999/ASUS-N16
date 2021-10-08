e60f9c97029750ee169ffd82e3c5d946103db480
keweifeng999 <63458898+keweifeng999@users.noreply.github.com>
2021-10-08 19:54:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,17 +15,5 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-      - name: 自定义插件主题。
-        run: |
-          cd package
-          mkdir openwrt-packages
-          cd openwrt-packages
-          # git clone https://github.com/kenzok8/openwrt-packages.git
-          # git clone https://github.com/kenzok8/small.git
-          git clone https://github.com/xiaorouji/openwrt-passwall.git
-          # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
-          # git clone https://github.com/AdguardTeam/AdGuardHome.git
-          # git clone https://github.com/ujincn/smartdns.git
-          # git clone https://github.com/ujincn/luci-app-smartdns-compat.git
-          # git clone https://github.com/honwen/luci-app-aliddns.git
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
