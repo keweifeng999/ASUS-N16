70d02e422177c783d8216b58c72d9cc32b18507d
keweifeng999 <63458898+keweifeng999@users.noreply.github.com>
2021-10-09 23:25:18 +0800

    Update test-part1.sh

--- test-part1.sh
+++ test-part1.sh
@@ -16,8 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
-echo 'src-git AdGuardHome https://github.com/AdguardTeam/AdGuardHome' >>feeds.conf.default
-echo 'src-git smartdns https://github.com/ujincn/smartdns' >>feeds.conf.default
-echo 'src-git luci-app-smartdns-compat https://github.com/ujincn/luci-app-smartdns-compat' >>feeds.conf.default
+# echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
+# echo 'src-git AdGuardHome https://github.com/AdguardTeam/AdGuardHome' >>feeds.conf.default
+# echo 'src-git smartdns https://github.com/ujincn/smartdns' >>feeds.conf.default
+# echo 'src-git luci-app-smartdns-compat https://github.com/ujincn/luci-app-smartdns-compat' >>feeds.conf.default
 #echo 'src-git luci-app-aliddns https://github.com/honwen/luci-app-aliddns' >>feeds.conf.default
