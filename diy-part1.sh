ce11ff1638510b1da5f3a6d3a32449594f6613b5
keweifeng999 <63458898+keweifeng999@users.noreply.github.com>
2021-10-27 14:15:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
